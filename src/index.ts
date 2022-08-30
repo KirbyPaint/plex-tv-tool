ebc69dba3f914a7c128c30032e26fa7d65efe93c
Ash Porter <ashley.porter@eyecuelab.com>
2022-08-29 22:32:30 -0700

    added records to db FINALLY

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { writeFileSync } from "fs";
 import axios from "axios";
 import dotenv from "dotenv";
+import { mapSeriesToDatabaseEntry } from "./utils/utils";
 
 dotenv.config();
 
-async function downloadAPIThings() {
+async function downloadAPIThings(): Promise<void> {
   const url = process.env.BASE_URL;
   console.log(`downloading api things`);
+  let count = 0;
   try {
     await axios
       .get(url + `series`, {
@@ -14,17 +16,19 @@ async function downloadAPIThings() {
           Authorization: `Bearer ` + process.env.BEARER,
         },
       })
-      .then((res) => {
+      .then(async (res) => {
         console.log(res);
+        count = await mapSeriesToDatabaseEntry(res.data.data);
         writeFileSync(`./series.json`, JSON.stringify(res.data));
       });
   } catch (e) {
     console.log(e);
   }
+  console.log(`${count} things downloaded`);
 }
 
 function main(): void {
-  // downloadAPIThings();
+  downloadAPIThings();
 }
 
 main();
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,35 @@
-export function mapSeriesToDatabaseEntry() {
-  console.log(`not done`);
+import { PrismaClient, Series } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function mapSeriesToDatabaseEntry(
+  entries: Series[]
+): Promise<number> {
+  let recordsInserted = 0;
+  // for each entry, create or update one record in the database
+  entries.forEach(async (entry) => {
+    try {
+      const create = {
+        ...entry,
+        nameTranslations: entry?.nameTranslations?.toString(),
+        overviewTranslations: entry?.overviewTranslations?.toString(),
+        aliases: entry?.aliases?.toString() ?? ``,
+        status: entry?.status?.toString(),
+        image: entry.image ?? ``,
+        firstAired: entry?.firstAired ?? undefined,
+        lastAired: entry?.lastAired ?? undefined,
+        averageRuntime: entry.averageRuntime ?? undefined,
+        originalCountry: entry.originalCountry ?? undefined,
+      };
+      await prisma.series.upsert({
+        where: { id: entry.id },
+        create,
+        update: create,
+      });
+      recordsInserted++;
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  return recordsInserted;
 }
