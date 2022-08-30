765a3ab1ec65d811cd55a20ca63a2300a901931d
Ash Porter <ashley.porter@eyecuelab.com>
2022-08-29 21:23:09 -0700

    add and enforce eslint

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ dotenv.config();
 
 async function downloadAPIThings() {
   const url = process.env.BASE_URL;
-  console.log("downloading api things");
+  console.log(`downloading api things`);
   try {
     await axios
-      .get(url + "series", {
+      .get(url + `series`, {
         headers: {
-          Authorization: "Bearer " + process.env.BEARER,
+          Authorization: `Bearer ` + process.env.BEARER,
         },
       })
       .then((res) => {
         console.log(res);
-        writeFileSync("./series.json", JSON.stringify(res.data));
+        writeFileSync(`./series.json`, JSON.stringify(res.data));
       });
   } catch (e) {
     console.log(e);
