24fc6552ead02fcf19e4f02a727b738cd066bd8c
Ash Porter <ashley.porter@eyecuelab.com>
2022-08-29 19:51:56 -0700

    add prisma and such

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import { writeFileSync } from "fs";
+
+function downloadAPIThings() {
+  console.log("downloading api things");
+  const fakeJSON = {
+    hello: "world",
+  };
+  writeFileSync("api-things.json", JSON.stringify(fakeJSON), "utf8");
+}
+
+function main(): void {
+  downloadAPIThings();
+}
+
+main();
